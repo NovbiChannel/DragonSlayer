1994e0af10fcc3b0e61cda9e879d5660a5df4243
novbi <nikita_ch@maximail.org>
2025-01-03 01:08:35 +0500

    * refactoring core InputType

--- core/src/main/kotlin/listeners/InputListeners.kt
+++ core/src/main/kotlin/listeners/InputListeners.kt
@@ -4,6 +4,7 @@ import com.github.kwhat.jnativehook.keyboard.NativeKeyEvent
 import com.github.kwhat.jnativehook.keyboard.NativeKeyListener
 import com.github.kwhat.jnativehook.mouse.NativeMouseEvent
 import com.github.kwhat.jnativehook.mouse.NativeMouseListener
+import macro.InputType
 import macro.Macro
 import notification.sendNotification
 
@@ -13,9 +14,15 @@ class KeyListener(
 ) : NativeKeyListener {
 
     override fun nativeKeyPressed(e: NativeKeyEvent) {
-        if (e.keyCode == macro.startStopKey) {
-            toggleMacroState(macro, runningMacros)
+        when (val inputType = macro.inputType) {
+            is InputType.KEYBOARD -> {
+                if (e.keyCode == inputType.value) {
+                    toggleMacroState(macro, runningMacros)
+                }
+            }
+            is InputType.MOUSE -> {}
         }
+
     }
 }
 
@@ -25,23 +32,33 @@ class MouseListener(
 ): NativeMouseListener {
 
     override fun nativeMousePressed(nativeEvent: NativeMouseEvent?) {
-        if (nativeEvent?.button == macro.startStopKey) {
-            toggleMacroState(macro, runningMacros)
+        when (val inputType = macro.inputType) {
+            is InputType.MOUSE -> {
+                if (nativeEvent?.button == inputType.value) {
+                    toggleMacroState(macro, runningMacros)
+                }
+            }
+            is InputType.KEYBOARD -> {}
         }
     }
 }
+
 
 private fun toggleMacroState(
     macro: Macro,
     runningMacros: MutableMap<Macro, Boolean>
 ) {
     val message: String
+    val key = when (val inputType = macro.inputType) {
+        is InputType.KEYBOARD -> inputType.value
+        is InputType.MOUSE -> inputType.value
+    }
     if (runningMacros[macro] == true) {
         runningMacros[macro] = false
         message = "Макрос '${macro.title}' остановлен"
     } else {
         runningMacros[macro] = true
-        message = "Макрос '${macro.title}' запущен, для остановки нажми ${NativeKeyEvent.getKeyText(macro.startStopKey)}"
+        message = "Макрос '${macro.title}' запущен, для остановки нажми ${NativeKeyEvent.getKeyText(key)}"
     }
     sendNotification(message)
 }
--- core/src/main/kotlin/macro/MacroCFG.kt
+++ core/src/main/kotlin/macro/MacroCFG.kt
@@ -4,15 +4,13 @@ import EventConfig
 import com.github.kwhat.jnativehook.keyboard.NativeKeyEvent
 import com.github.kwhat.jnativehook.mouse.NativeMouseEvent
 import min
-import sec
 import java.awt.event.KeyEvent
-import java.awt.event.MouseEvent
 
 data class Macro(
     val title: String,
     val description: String,
     val comment: String,
-    val startStopKey: Int,
+    val inputType: InputType,
     val loopType: LoopType,
     val keys: List<EventConfig>
 )
@@ -23,11 +21,16 @@ sealed class LoopType {
     data class CUSTOM(val repetitions: Int): LoopType()
 }
 
+sealed class InputType {
+    data class KEYBOARD(val value: Int): InputType()
+    data class MOUSE(val value: Int): InputType()
+}
+
 val sumF1toF3 = Macro(
-    title = "Sum solo targer without balance",
+    title = "Соло таргет без баланса",
     description = "Нажатие от f1 до f3 с минимальной задержкой",
     comment = "Yours соло таргет фарм без баланса",
-    startStopKey = NativeMouseEvent.BUTTON4,
+    inputType = InputType.MOUSE(NativeMouseEvent.BUTTON4),
     loopType = LoopType.INFINITE,
     keys = listOf(
         EventConfig(KeyEvent.VK_F1),
@@ -37,10 +40,10 @@ val sumF1toF3 = Macro(
 )
 
 val sumF1toF4 = Macro(
-    title = "Sum solo target with a balance",
+    title = "Соло таргет + баланс",
     description = "Нажатие от f1 до f3 с минимальной задержкой, f4 - интервал нажатия 1 минута",
     comment = "Yours соло таргет, фарм Алигаторов",
-    startStopKey = NativeMouseEvent.BUTTON5,
+    inputType = InputType.MOUSE(NativeMouseEvent.BUTTON5),
     loopType = LoopType.INFINITE,
     keys = listOf(
         EventConfig(KeyEvent.VK_F1),
@@ -50,10 +53,10 @@ val sumF1toF4 = Macro(
     )
 )
 val sumF1toF6AOE = Macro(
-    title = "Sum AOE farm with a balance",
+    title = "АОЕ + баланс",
     description = "Нажатие от f1 до f5 с минимальной задержкой, f6 - интервал нажатия 3 минуты",
     comment = "Yours AOE farm",
-    startStopKey = NativeKeyEvent.VC_BACKQUOTE,
+    inputType = InputType.KEYBOARD(NativeKeyEvent.VC_BACKQUOTE),
     loopType = LoopType.INFINITE,
     keys = listOf(
         EventConfig(KeyEvent.VK_F1),
@@ -61,6 +64,6 @@ val sumF1toF6AOE = Macro(
         EventConfig(KeyEvent.VK_F3),
         EventConfig(KeyEvent.VK_F4),
         EventConfig(KeyEvent.VK_F5),
-        EventConfig(KeyEvent.VK_F6, interval = 3.min)
+        EventConfig(KeyEvent.VK_F6, interval = 1.min)
     )
 )
